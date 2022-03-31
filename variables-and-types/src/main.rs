56cf64610f002799968fcd611295d77139f1f5e4
wurikiji <wurikiji@gmail.com>
2022-03-31 20:36:51 +0900

    3.3. Data types

--- variables-and-types/src/main.rs
+++ variables-and-types/src/main.rs
@@ -8,7 +8,16 @@ fn main() {
     let x = x + 1;
     {
         let x = x * 2;
+        // array type
+        let arr: [i32; 5] = [1, 2, 3, 4, 5];
+        let arr = [arr[3]; 5];
+        // tuple type
+        let tup: (i32, i32, i32) = (x * 2, arr[0] * 3, x + 1);
+        // tuple destruction
+        let (x, ..) = tup;
+
         println!("The value of x in the inner scope is: {}", x);
     }
     println!("The value fo x is: {}", x);
+    println!("Constant is {}", THREE_HOURS_IN_SECONDS);
 }
