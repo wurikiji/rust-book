42456c4a45c048fd2f42d8534f61950c8344d913
wurikiji <wurikiji@gmail.com>
2022-03-31 11:00:50 +0900

    3.1. variables and mutability

---
+++ variables/src/main.rs
@@ -0,0 +1,14 @@
+const THREE_HOURS_IN_SECONDS: u32 = 3 * 60 * 60;
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let x = x + 1;
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+    println!("The value fo x is: {}", x);
+}
