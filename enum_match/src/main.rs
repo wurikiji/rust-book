7f67e34aff5a6e3fc7635fd22d3b092bade2fa5d
wurikiji <wurikiji@gmail.com>
2022-04-08 17:15:13 +0900

    6.2 and 6.3. match and if let control flow

---
+++ enum_match/src/main.rs
@@ -0,0 +1,66 @@
+#[derive(Debug, Clone)]
+enum UsState {
+    Alabama,
+    Alaska,
+    California,
+    // -- skip --
+}
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    let mut count = 0;
+    if let Coin::Quarter(ref state) = coin {
+        println!("State quarter from {:?}", state);
+    } else {
+        count += 1;
+    }
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
+
+fn main() {
+    value_in_cents(Coin::Quarter(UsState::California));
+    value_in_cents(Coin::Penny);
+    let five = Some(5);
+    let _six = plus_one(five).unwrap();
+    let _none = plus_one(None);
+
+    let dice_roll = 9;
+    match dice_roll {
+        3 => add_fancy_hat(),
+        7 => remove_fancy_hat(),
+        8 => (), // nothing happend by unit value
+        other => move_player(other),
+    }
+
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The maximum is configured to be {}", max);
+    }
+}
+
+fn add_fancy_hat() {}
+fn remove_fancy_hat() {}
+fn move_player(num_spaces: u8) {}
