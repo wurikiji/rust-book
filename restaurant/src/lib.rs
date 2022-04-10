8ae3a21f36e25ac0d3131a6cc54b5507c02d2aca
wurikiji <wurikiji@gmail.com>
2022-04-10 20:04:31 +0900

    7.5 Separating Modules into Different Files

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -6,18 +6,7 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
-
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        fn seat_at_table() {}
-    }
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 mod back_of_house {
     // pub struct's fields are not pub by default
