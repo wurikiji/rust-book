c15e9d9e0ee51f3e36b7fc462958cc84cd4f0b1c
wurikiji <wurikiji@gmail.com>
2022-04-24 21:50:55 +0900

    13.3 Improving IO project

--- 12.An-IO-Project_Building_a_Command_Line_Program/src/lib.rs
+++ 12.An-IO-Project_Building_a_Command_Line_Program/src/lib.rs
@@ -7,12 +7,16 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next(); // ignore the executable file name
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config {
             query,
