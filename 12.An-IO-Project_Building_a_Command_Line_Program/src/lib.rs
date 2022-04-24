9828dd8e77d73e848da07b210d541cb4ef1b84fd
wurikiji <wurikiji@gmail.com>
2022-04-24 14:37:40 +0900

    12.4. Developing the library's functionality with TDD

--- 12.An-IO-Project_Building_a_Command_Line_Program/src/lib.rs
+++ 12.An-IO-Project_Building_a_Command_Line_Program/src/lib.rs
@@ -18,6 +18,30 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filename)?;
-    println!("With text:\n{}", contents);
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
     Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick Three.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
