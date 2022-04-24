2b44cba7f33362be82df8fd4d9f52e4b8abb5c8f
wurikiji <wurikiji@gmail.com>
2022-04-24 11:55:03 +0900

    12.3. Refactoring to Improve Modularity and Error Handling

--- 12.An-IO-Project_Building_a_Command_Line_Program/src/main.rs
+++ 12.An-IO-Project_Building_a_Command_Line_Program/src/main.rs
@@ -1,14 +1,19 @@
-use std::{env, fs};
+use std::{env, process};
 
+use grep_project::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    println!("With text:\n{}", contents);
+    if let Err(e) = grep_project::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
