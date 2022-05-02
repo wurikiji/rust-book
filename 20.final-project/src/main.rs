64bb9b9f7843c33c520f864af57dab8811e5ba1c
wurikiji <wurikiji@gmail.com>
2022-05-02 22:00:28 +0900

    20.2 Turning Our Single-Threaded Server into a Multithreaded Server

--- 20.final-project/src/main.rs
+++ 20.final-project/src/main.rs
@@ -2,15 +2,23 @@ use std::{
     fs,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+
+use final_project::ThreadPool;
 
 fn main() {
     let listener =
         TcpListener::bind("127.0.0.1:7878").expect("can not bind to address 127.0.0.1:7878");
+    let pool = ThreadPool::new(4).unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -20,8 +28,12 @@ fn handle_connection(mut stream: TcpStream) {
     // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
         ("HTTP/1.1 200 OK", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND", "404.html")
